24f0584af30d2b8408b36b7db4ae0d5f0fab4eef
semihkokcu <SemihKokcu/Projects>
2021-05-18 21:14:05 +0300

    Add project files.

---
+++ RecapProject1/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using RecapProject1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace RecapProject1
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+  
